9f31fe8c71a075c1dfb62660b9052d9c0095a297
Zsofia <you@example.com>
2023-03-29 23:21:45 +0200

    added an other section to home page

--- src/components/HP_Section.jsx
+++ src/components/HP_Section.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const HP_Section = () => {
   return (
-    <div className='symbols'>
-      <span className="material-symbols-outlined"></span>
-      
-    </div>
-  
-  )
-}
+    // <div className='symbols'>
+    //   <span className="material-symbols-outlined"></span>
+    // </div>
+    <section className="section-container dark">
+      <div className="section-left">
+        <div className="section-left-icons">
+            <img
+              src="/real_estate_agent.png"
+              alt="Hands holding house"
+              width="52px"
+            />
+            <div className="real-estate">
+              <h3>Best selection</h3>
+              <p>
+                Best selection of student accommodations.
+                <br />
+                Never been easier to find a home that's
+                <br />
+                right for you.
+              </p>
+          </div>
+        </div>
+          <div className="section-left-icons">
+            <img 
+              src="/icon_favorites.png" 
+              alt="Favorits" 
+              width="52px" />
+             <div className="favorites">
+                <h3>Your favorite</h3>
+                <p>
+                  Shortlist your favourite properties and send <br />
+                  enquiries in one click.
+                </p>
+            </div>
+          </div>
+        
+      </div>
+      <div className="section-right">
+        <img className="img-person" src="/person.png" alt="" />
+      </div>
+    </section>
+  );
+};
 
 export default HP_Section;
