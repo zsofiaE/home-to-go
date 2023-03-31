bb67fd76ed5b6657e8716f5be8dd395e564376fd
Zsofia <you@example.com>
2023-03-31 15:24:02 +0200

    added useEffect to CitiesContext

--- src/Context/CitiesContext.jsx
+++ src/Context/CitiesContext.jsx
@@ -1,18 +1,23 @@
 import { createContext, useEffect, useState } from 'react'
 
 export const CitiesContext = createContext();
-constCitiesContextProvider = ({children}) => {
+const CitiesContextProvider = ({children}) => {
     const[cities, setCities] = useState([]);
+    let [page, setPage] = useState(1);
     const url = `https://unilife-server.herokuapp.com/cities?limit=10&page=${page}`;
 
-    useEffect() => {
+    useEffect(() => {
         fetch(url)
         .then(arr => arr.json())
         .then(function(res) {
 
-            return setCities(() => [...cities,res.response])
+            return setCities(() => [...cities,...res.response])
         });
-    }; []};
+    }, []);
+
+
+
+    console.log(cities)
 
   return (
     <CitiesContext.Provider value={{cities, setProducts}}>
@@ -20,6 +25,7 @@ constCitiesContextProvider = ({children}) => {
     </CitiesContext.Provider>
    
   )
+}
 
 
 export default CitiesContextProvider;
