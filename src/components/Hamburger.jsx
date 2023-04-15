df99de9743537c19d6d87f81869b05dced1178ed
Zsofia <you@example.com>
2023-04-15 10:11:46 +0200

    first try hamburger

---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,33 @@
+const Hamburger = ({isOpen}) => {
+    return (
+        <>
+        <div className="hamburger">
+            <div className="burger burger1" />
+            <div className="burger burger2" />
+            <div className="burger burger3" />
+        </div>
+
+        <style>{`
+            .hamburger{
+            width: 32px;
+            height: 32px;
+            display: flex;
+            justify-content: space-around;
+            flex-flow: column nowrap;
+            z-index: 10;
+            }
+        
+        .burger{
+            width: 32px;
+            height: 4px;
+            border-radius: 10px;
+            background-color: white;
+            transform-origin: 1px;
+            transition: all 0.3s linear;
+            }            
+        `}</style>
+        </>
+      );
+}
+ 
+export default Hamburger;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,33 @@
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Hamburger from "./Hamburger";
+import { useState } from "react";
 
 const Navbar = () => {
+const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+const handleToggle = () => {
+    setHamburgerOpen(!hamburgerOpen)
+}
+
     return (
-        <nav className="navigation">
+        <nav className="navbar">
             <Link to="/">
             <div className="navbar-logo">
                 <img className="logo-icon" src="/icons-house.png" alt="house" />
                 <h3 className="logo"> home to go</h3>
             </div>
             </Link>
-            <div className="navbar header">
+            <div className="navigation">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="shortlist"><FontAwesomeIcon icon={faHeart} className="fa-brands" /> Shortlist</NavLink>
                 <NavLink to="contact"><FontAwesomeIcon icon={faEnvelope} className="fa-brands" /> Contact us</NavLink>
             </div>
+            <div className="hamburger" onClick={handleToggle}>
+                    <Hamburger isOpen={hamburgerOpen} />
+            </div>
+
         </nav>
     )
 };
